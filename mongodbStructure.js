aa40bbe6daed05a72defd5cc23c603c63fa72f94
Tiago Vota Cucco <tiagovotacucco@gmail.com>
2022-02-09 11:21:58 -0300

    make mongodb structure schema; improve eslintrc.json

---
+++ mongodbStructure.js
@@ -0,0 +1,63 @@
+const collections = [
+	'sessions',
+	'users',
+	'products',
+	'types',
+	'carts',
+	'checkout'
+]
+
+
+const sessionDocument = {
+	user_id: 'this is an ObjectId from user',
+	token: 'this is an uuid token'
+}
+
+const userDocument = {
+	name: 'username string',
+	email: 'user email',
+	password: 'hashed password',
+	image: 'user image by url'
+}
+
+const productDocument = {
+	name: 'pokémon name',
+	price: 'pokémon price',
+	types_id: [
+		'type id from types, is an ObjectId',
+		'type id from types, is an ObjectId',
+	]
+}
+
+const typeDocument = {
+	name: 'type name'
+}
+
+const cartDocument = {
+	user_id: 'this is an ObjectId from user',
+	products: [
+		{
+			product_id: 'product id from products, is an ObjectId',
+			quantity: 'number of products'
+		},
+		{
+			product_id: 'product id from products, is an ObjectId',
+			quantity: 'number of products'
+		}
+	]
+}
+
+const checkoutDocument = {
+	user_id: 'this is an ObjectId from user',
+	products: [
+		{
+			product_id: 'product id from products, is an ObjectId',
+			quantity: 'number of products'
+		},
+		{
+			product_id: 'product id from products, is an ObjectId',
+			quantity: 'number of products'
+		}
+	],
+	date: 'finalize order date in timestamp'
+}
